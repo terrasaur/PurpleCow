82f8f14108a241206ca3f5ea70167293c8dc9f34
Teresa Oswald <terrasaur57@gmail.com>
2022-06-28 18:15:43 -0400

    ok it works now

--- src/main/java/cow/item/ItemApplication.java
+++ src/main/java/cow/item/ItemApplication.java
@@ -2,15 +2,8 @@ package cow.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-@EnableJpaRepositories("cow.item")
-@ComponentScan(basePackages = { "cow.item" })
-@EntityScan("cow.item")  
+@SpringBootApplication
 public class ItemApplication {
 
 	public static void main(String[] args) {
